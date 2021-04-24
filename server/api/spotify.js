e2a2d938b6d63c2c5eb623025e04a85a58af9bed
kylelainez <kyleadrianlainez@gmail.com>
2021-04-24 11:10:10 -0700

    Move values to env

--- server/api/spotify.js
+++ server/api/spotify.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const SpotifyWebApi = require('spotify-web-api-node');
 
 router.post('/login', (req, res) => {
     const code = req.body.code;
-    const URL = 'https://accounts.spotify.com/api/token';
-
-    axios.post(url, {
-        params: {
-            grant_type = 'authorization_code',
-            code,
-            redirect_uri = 'http://localhost:3000',
-            client_id: 'fb453ef5b2ef4b788dfcb3a4839673b4',
-            client_secret: '954cc62f7bf84e20a8e5df0d7149b590'
-        }
-    }).then(data => {
-        console.log(data);
+    
+    const spotifyWebApi = new SpotifyWebApi({
+        redirectUri : process.env.redirectUri,
+        clientId: process.env.clientId,
+        clientSecret: process.env.clientSecret
     })
+
+    spotifyWebApi
+        .authorizationCodeGrant(code)
+        .then( data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.sendStatus(400);
+        });
 });
 
 module.exports = router;
